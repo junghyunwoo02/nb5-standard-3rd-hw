4044a60c8791cbbf74742e0498f373ed11ee4cb2
junghyunwoo02 <jungilbung@naver.com>
2024-05-21 12:17:52 +0900

    ex1 finish

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,27 @@ export default function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  // const chooseStudents = (minAge = 18) =>
+  //   students.filter((student) => student.age >= minAge);
+  // const filteredStudents = chooseStudents();
+  const filteredStudents = students.filter((student) => student.age >= 18);
+
+  const handleClick = ({ age, grade }) => {
+    alert(`나이 : ${age}, 성적 : ${grade}`);
+  };
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+        {
+          /* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */
+          filteredStudents.map((student) => (
+            <li key={student.name} onClick={() => handleClick(student)}>
+              <p>이름 : {student.name}</p>
+            </li>
+          ))
+        }
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
     </div>
