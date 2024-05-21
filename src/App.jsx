17a1cfcf9aa1705781a0e0b2ac86414e08c529ca
junghyunwoo02 <jungilbung@naver.com>
2024-05-21 14:57:18 +0900

    ex2 완료

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,39 @@ function App() {
 
   // TODO: filter를 사용하여 minAge 이상의 학생들만 선택하세요.
   // const filteredStudents = ...;
+  const filteredStudents = students.filter((student) => student.age >= minAge);
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value);
+    console.log("test1");
+    if (!isNaN(value)) {
+      // 숫자인 경우에만 최소 나이 업데이트
+      setMinAge(value);
+      console.log("test2");
+      return;
+    }
+    console.log("test3");
+  };
 
   return (
     <div>
       <h1>학생 목록</h1>
       다음 나이 이상의 학생목록만 출력해요 :{" "}
       {/* TODO: input에 입력된 값(숫자) 이상의 나이를 가진 학생들만 출력하세요. */}
-      <input type="number" /> 살 이상
+      <input type="number" value={minAge} onChange={handleInputChange} /> 살
+      이상
       <ul>
-        {/* TODO: map을 사용하여 필터링된 학생들의 정보를 표시하세요. 동명이인은 없다고 가정합니다. ex) 홍길동 - Age: 24, Grade: A */}
+        {
+          /* TODO: map을 사용하여 필터링된 학생들의 정보를 표시하세요. 동명이인은 없다고 가정합니다. ex) 홍길동 - Age: 24, Grade: A */
+          filteredStudents.map((student) => (
+            <li key={student.name}>
+              <p>
+                이름 : {student.name} - Age : {student.name} Grade :{" "}
+                {student.grade}
+              </p>
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
